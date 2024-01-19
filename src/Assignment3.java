a68b9d2bdd3092bf3ad583a7cbdaaaeb96399c19
Megan Gould <meganlorrainegould@gmail.com>
2024-01-19 13:09:25 -0500

    adding changes

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Assignment3 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+     System.out.println("Welcome to metric converter!");
+     System.out.println("Please choose a conversion type:");
+     System.out.println("1. Kilometers to Miles");
+     System.out.println("2. Meters to Feet");
+     System.out.println("3. Celsius to Farenheit");
+     System.out.println("4. Kilogram to Pound");
+     System.out.println("Or input 'exit' to close the program.");
+      int choice = scanner.nextInt();
+      switch (choice) {
+        case 1:
+
+      }
+      scanner.close();
     }
 }
